52cd80b276276a4e90f3edcaf99be9bca789973b
D3atHpR <kastiel103@gmail.com>
2021-12-05 00:51:08 -0300

    1.5

--- CompilerDNsK/CompilerDNsK/Program.cs
+++ CompilerDNsK/CompilerDNsK/Program.cs
@@ -49,7 +49,7 @@ enum SyntaxKind
     BinaryExpression
 }
 
-class SyntaxToken
+class SyntaxToken: SyntaxNode
 {
     public SyntaxToken(SyntaxKind kind, int position, string text, object value)
     {
@@ -58,10 +58,15 @@ class SyntaxToken
         Text = text;
         Value = value;
     }
-    public SyntaxKind Kind { get; }
+    public override SyntaxKind Kind { get; }
     public int Position { get; }
     public string Text { get; }
     public object Value { get; }
+
+    public override IEnumerable<SyntaxNode> GetChildren()
+    {
+        return Enumerable.Empty<SyntaxNode>();
+    }
 }
 
 class Lexer
@@ -137,6 +142,10 @@ class Lexer
 abstract class SyntaxNode 
 {
     public abstract SyntaxKind Kind { get; }
+
+    public  abstract IEnumerable<SyntaxNode> GetChildren();
+
+
 }
 
  abstract class ExpressionSyntax : SyntaxNode
@@ -151,11 +160,16 @@ sealed class NumberExpressionSyntax : ExpressionSyntax
 
     public override SyntaxKind Kind => SyntaxKind.NumberExpression;
     public SyntaxToken NumberToken { get; }
+
+    public override IEnumerable<SyntaxNode> GetChildren()
+    {
+        yield return NumberToken;
+    }
 }
 
 sealed class BinaryExpressionSyntax: ExpressionSyntax
 {
-    public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxNode operatorToken, ExpressionSyntax right )
+    public BinaryExpressionSyntax(ExpressionSyntax left, SyntaxToken operatorToken, ExpressionSyntax right )
     {
         Left = left;
         OperatorToken = operatorToken;
@@ -165,8 +179,15 @@ sealed class BinaryExpressionSyntax: ExpressionSyntax
     public override SyntaxKind Kind => SyntaxKind.BinaryExpression;
 
     public ExpressionSyntax Left { get; }
-    public SyntaxNode OperatorToken { get; }
+    public SyntaxToken OperatorToken { get; }
     public ExpressionSyntax Right { get; }
+
+    public override IEnumerable<SyntaxNode> GetChildren()
+    {
+        yield return Left;
+        yield return OperatorToken;
+        yield return Right;
+    }
 }
 
 
@@ -201,4 +222,39 @@ class Parser
     }
     private SyntaxToken Current => Peek(0);
 
+
+    private SyntaxToken NextToken()
+    {
+        var current = Current;
+        _position++;
+        return current;
+    }
+    private SyntaxToken Match(SyntaxKind kind)
+    {
+        if (Current.Kind == kind)
+        {
+            return NextToken();
+        }
+        return new SyntaxToken(kind, Current.Position, null, null);
+    }
+
+    public ExpressionSyntax Parse()
+    {
+        var left = ParsePrimaryExpression();
+
+        while (Current.Kind == SyntaxKind.PlusToken || Current.Kind == SyntaxKind.MinusToken)
+        {
+            var operatorToken = NextToken();
+            var right = ParsePrimaryExpression();
+            left = new BinaryExpressionSyntax(left, operatorToken, right);
+        }
+
+        return left;
+    }
+
+    private ExpressionSyntax ParsePrimaryExpression()
+    {
+        var numberToken = Match(SyntaxKind.NumberToken);
+        return new NumberExpressionSyntax(numberToken);
+    }
 }
